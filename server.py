c4f23f7807c76f52271fe939280ce78c7bd541ef
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2019-08-12 14:34:53 -0400

    Provide loss probability from server side

--- server.py
+++ server.py
@@ -18,7 +18,8 @@ def main():
     logging.basicConfig(level=logging.DEBUG, 
             format='%(levelname)s: %(message)s')
 
-    receiver = swp.SWPReceiver((settings.hostname, settings.port))
+    receiver = swp.SWPReceiver((settings.hostname, settings.port), 
+            settings.loss_probability)
     while True:
         data = receiver.recv().decode()
         print('%s' % data)
